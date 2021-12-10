916750d9037edd6eddf2de4315416feb0f78bf88
TheRealGWJosh <exojosh50@gmail.com>
2021-12-10 12:45:41 -0800

    2 files modified

--- AvatarAttribute/dist/plugin.js
+++ AvatarAttribute/dist/plugin.js
@@ -1,1 +1,1 @@
-(function(c,o,s,r){"use strict";var e=t=>{t.persist.store,t.persist.ghost,r.webpack.findByDisplayName("PeopleListItem"),r.webpack.findByProps("getPrimaryColorForAvatar")};return e})(cumcord.utils.logger,cumcord.utils,cumcord.patcher,cumcord.modules);
+(function(e,i,o,r){"use strict";var s=t=>{t.persist.store,t.persist.ghost;const c=r.webpack.findByProps("img")[0].src.split("/")[4];r.webpack.setAttribute("data-user-id",c),r.webpack.findByProps("getPrimaryColorForAvatar")};return s})(cumcord.utils.logger,cumcord.utils,cumcord.patcher,cumcord.modules);
