c519cb403e190f8d3bcce6206e5b96319e17938c
TheRealGWJosh <exojosh50@gmail.com>
2021-12-10 13:03:27 -0800

    3 files modified

--- AvatarAttribute/avatarattribute.js
+++ AvatarAttribute/avatarattribute.js
@@ -11,7 +11,7 @@ export default (data) => {
   const store = data.persist.store;
   const ghostStore = data.persist.ghost;
 
-  const userId = webpack.findByProps('img')[0].src.split('/')[4];
+  const userId = webpack.findByProps('img')[0].setAttribute.split('/')[4];
   const el = webpack.setAttribute('data-user-id', userId);
   const getPrimaryColorForAvatar = webpack.findByProps(
     "getPrimaryColorForAvatar"
